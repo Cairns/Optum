87648274597ff0cba97869efe8f674c60b10197d
Andrew Cairns <acairns@liaisonfs.com>
2025-09-29 14:34:05 +0100

    Add vending machine unit tests Add test to display INSERT COIN when no coins are inserted

---
+++ VendingMachine.Tests/VendingMachineUnitTests.cs
@@ -0,0 +1,18 @@
+﻿namespace VendingMachine.Tests
+{
+    public class VendingMachineTests
+    {
+        [Fact]
+        public void Display_Should_Show_Insert_Coin_When_No_Coins_Are_Inserted()
+        {
+            // Arrange
+            var vendingMachine = new VendingMachine();
+
+            // Act
+            var displayMessage = vendingMachine.Display();
+
+            // Assert
+            Assert.Equal("INSERT COIN", displayMessage);
+        }
+    }
+}
