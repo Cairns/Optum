af01fcdae072d918d53f73746f236d61e2f4e944
Andrew Cairns <acairns@liaisonfs.com>
2025-09-29 15:28:25 +0100

    Add InsertCoin implementation to pass tests

--- VendingMachine/VendingMachine.cs
+++ VendingMachine/VendingMachine.cs
@@ -2,9 +2,19 @@
 {
     public class VendingMachine
     {
+        public int CurrentAmount { get; private set; }
+
+        public VendingMachine() { }
+
         public static string Display()
         {
             return "INSERT COIN";
+        }
+
+        public void InsertCoin(Coin coin)
+        {
+            //TODO: Validate coin
+            CurrentAmount += coin.Value;
         }
     }
 }
