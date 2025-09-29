9635167087d2cac9a6788026483023bccf92ee12
Andrew Cairns <acairns@liaisonfs.com>
2025-09-29 14:45:40 +0100

    Write failing test for accepting gbp coinage

--- VendingMachine.Tests/VendingMachineUnitTests.cs
+++ VendingMachine.Tests/VendingMachineUnitTests.cs
@@ -14,5 +14,18 @@ namespace VendingMachine.Tests
             // Assert
             Assert.Equal("INSERT COIN", displayMessage);
         }
+
+        [Theory]
+        [InlineData(Money.GBP.Coinage.OnePence)]
+        public void Insert_Coin_Should_Update_Display_Message(Money.GBP.Coinage coin)
+        {
+            // Arrange
+            var vendingMachine = new VendingMachine();
+            // Act
+            vendingMachine.InsertCoin(coin);
+            var displayMessage = VendingMachine.Display();
+            // Assert
+            Assert.Equal("£0.01", displayMessage);
+        }
     }
 }
