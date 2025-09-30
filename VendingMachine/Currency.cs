6a58c582cfe2f295594b8a53debf74275381f593
Andrew Cairns <acairns@liaisonfs.com>
2025-09-30 11:34:30 +0100

    Update currency to include centesimal symbol

--- VendingMachine/Currency.cs
+++ VendingMachine/Currency.cs
@@ -1,4 +1,4 @@
-﻿namespace VendingMachine
+namespace VendingMachine
 {
     /// <summary>
     /// Represents a currency with an ISO 4217 code and a symbol.
@@ -17,17 +17,23 @@
         /// </summary>
         public string Symbol { get; }
 
-        public Currency(string code, string symbol)
+        /// <summary>
+        /// Gets the symbol used to represent cents in currency formatting.
+        /// </summary>
+        public string CentSymbol { get; }
+
+        public Currency(string code, string symbol, string centSymbol)
         {
             Code = code;
             Symbol = symbol;
+            CentSymbol = centSymbol;
         }
 
         /// <summary>
         /// Represents the British Pound Sterling currency.
         /// </summary>
         /// <remarks>This static field provides a predefined instance of the <see cref="Currency"/> class
-        /// for the British Pound Sterling, with the ISO 4217 code "GBP" and the symbol "£".</remarks>
-        public static Currency GBP = new("GBP", "£");
+        /// for the British Pound Sterling, with the ISO 4217 code "GBP" and the symbol "£" and the cent symbol "p".</remarks>
+        public static Currency GBP = new("GBP", "£", "p");
     }
 }
