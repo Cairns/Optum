edf9f81871c7399f0644f0923ef7ffb1882af1c6
Andrew Cairns <acairns@liaisonfs.com>
2025-09-30 10:43:30 +0100

    Add summary comments to interface Add ValidCoins collection getter to validation strategy interface

--- VendingMachine/IValidationStrategy.cs
+++ VendingMachine/IValidationStrategy.cs
@@ -1,9 +1,22 @@
-﻿namespace VendingMachine
+namespace VendingMachine
 {
     public interface IValidationStrategy
     {
+        /// <summary>
+        /// Gets the currency for which this instance is valid.
+        /// </summary>
         public Currency ValidFor { get; }
 
+        /// <summary>
+        /// Gets the collection of valid coins for the specified currency.
+        /// </summary>
+        public IReadOnlyCollection<Coin> ValidCoins { get; }
+
+        /// <summary>
+        /// Determines whether the specified coin is valid.
+        /// </summary>
+        /// <param name="coin">The coin to validate. Must not be <see langword="null"/>.</param>
+        /// <returns><see langword="true"/> if the specified coin is valid; otherwise, <see langword="false"/>.</returns>
         public bool IsValid(Coin coin);
     }
 }
