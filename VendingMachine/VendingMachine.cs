5a9309bf07f7c0d9af88cbb4fa1593b827960776
Andrew Cairns <acairns@liaisonfs.com>
2025-09-30 16:22:06 +0100

    Refactor to extract messages to separate class

--- VendingMachine/VendingMachine.cs
+++ VendingMachine/VendingMachine.cs
@@ -71,12 +71,4 @@ namespace VendingMachine
             //TODO: Implement this method
         }
     }
-
-    public class VendingMachineMessages
-    {
-        public const string InsertCoin = "INSERT COIN";
-        public const string ThankYou = "THANK YOU";
-        public const string SoldOut = "SOLD OUT";
-        public const string ExactChangeOnly = "EXACT CHANGE ONLY";
-    }
 }
---
+++ VendingMachine/VendingMachineMessages.cs
@@ -0,0 +1,10 @@
+namespace VendingMachine
+{
+    public class VendingMachineMessages
+    {
+        public const string InsertCoin = "INSERT COIN";
+        public const string ThankYou = "THANK YOU";
+        public const string SoldOut = "SOLD OUT";
+        public const string ExactChangeOnly = "EXACT CHANGE ONLY";
+    }
+}
